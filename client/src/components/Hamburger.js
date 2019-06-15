ecdb582c2a59ca072af3bcad2c363d844d9c694e
brianplynn <brnlynn889@gmail.com>
2019-06-14 21:39:49 -0700

    more responsiveness

--- client/src/components/Hamburger.js
+++ client/src/components/Hamburger.js
@@ -1,32 +1,48 @@
 import React from "react";
-import { slide as Menu } from 'react-burger-menu'
-import { Link } from "react-router-dom"
+import { slide as Menu } from "react-burger-menu";
+import { Link } from "react-router-dom";
 import { logOut } from "../functions";
 import "./Nav.css";
 
 const Hamburger = ({ setSection }) => {
   return (
-    <Menu className={ "hamburger-menu" } width={ '80%' }disableAutoFocus right>
+    <Menu className={"hamburger-menu"} width={"60%"} disableAutoFocus right>
       <Link to="/nutrition">
-        <span className="link dim pointer outline-0 white dib ml5 mb2 mr3"
-            title="nutrition"
-            onClick={setSection}>Nutrition</span>
+        <span
+          className="link dim pointer outline-0 white dib ml1 mb2 mr3"
+          title="nutrition"
+          onClick={setSection}
+        >
+          Nutrition
+        </span>
       </Link>
       <Link to="/sleep">
-        <span className="link dim pointer outline-0 white dib ml5 mb2 mr3"
-            title="sleep"
-            onClick={setSection}>Sleep</span>
+        <span
+          className="link dim pointer outline-0 white dib ml1 mb2 mr3"
+          title="sleep"
+          onClick={setSection}
+        >
+          Sleep
+        </span>
       </Link>
       <Link to="/exercise">
-        <span className="link dim pointer outline-0 white dib ml5 mb2 mr3"
-            title="exercise"
-            onClick={setSection}>Exercise</span>
+        <span
+          className="link dim pointer outline-0 white dib ml1 mb2 mr3"
+          title="exercise"
+          onClick={setSection}
+        >
+          Exercise
+        </span>
       </Link>
-        <span className="link dim pointer outline-0 white dib ml5 mb2 mr3"
-            title="exercise"
-            onClick={logOut}>Log Out</span>
+      <span
+        className="link dim pointer outline-0 white dib ml1 mb2 mr3"
+        title="exercise"
+        onClick={logOut}
+      >
+        Log Out
+      </span>
     </Menu>
   );
-}
+};
 
 export default Hamburger;
--- client/src/components/Nutrition/NutritionTable.js
+++ client/src/components/Nutrition/NutritionTable.js
@@ -1,66 +1,98 @@
-import React from 'react';
+import React from "react";
+import "./NutritionTable.css";
 
 const NutritionTable = ({ dailyFoods, deleteFood, addAnotherFood }) => {
-	const sumNutrient = (nutrient) => {
-		return dailyFoods[0] ? dailyFoods.map( food => Number(food[nutrient]))
-						 .reduce( (acc, currVal) => acc + currVal)
-					   : ""
-	}
+	const sumNutrient = nutrient => {
+		return dailyFoods[0]
+			? dailyFoods
+					.map(food => Number(food[nutrient]))
+					.reduce((acc, currVal) => acc + currVal)
+			: "";
+	};
 	return (
-		<div className="pa4">
-		  <div className="overflow-auto">
-		  	<h1 className="tc white mb5">Your Daily Nutrition History</h1>
-		    <table className="f6 w-100 mw8 center" cellSpacing="0">
-		      <thead>
-		        <tr>
-		          <th className="fw6 tc white bb pb3 pr3">Food</th>
-		          <th className="fw6 tc white bb pb3 pr3">Calories</th>
-		          <th className="fw6 tc white bb pb3 pr3">Fat</th>
-		          <th className="fw6 tc white bb pb3 pr3">Carbs</th>
-		          <th className="fw6 tc white bb pb3 pr3">Protein</th>
-		        </tr>
-		      </thead>
-		      <tbody className="lh-copy">
-		        {dailyFoods.map( (food, i) => {
-				       return <tr key={i}>
-				          <td className="pv3 tc pr3 bb b--white">{food.food}</td>
-				          <td className="pv3 tc pr3 bb b--white">{food.calories}</td>
-				          <td className="pv3 tc pr3 bb b--white">{food.fat}</td>
-				          <td className="pv3 tc pr3 bb b--white">{food.carbs}</td>
-				          <td className="pv3 tc pr3 bb b--white">{food.protein}</td>
-				       	  <td className="pv3 tc pr3">
-				       	  <button className="br-100 bw1 b pointer ba b--washed-red bg-transparent washed-red"
-			       	  			value={i}
-			       	  			data-id={food.food_id} 
-			       	  			onClick={deleteFood}>x</button></td>
-				        </tr>
-				        ;
-		    		})
-		        }
-		        <tr>
-		        	<td className="pv3 b tc pr3 bt b--white">Daily Total</td>
-				    <td className="pv3 b tc pr3 bt b--white">
-				    	{sumNutrient("calories")}
-				    </td>
-				    <td className="pv3 b tc pr3 bt b--white">
-				    	{sumNutrient("fat")}
-				    </td>
-				    <td className="pv3 b tc pr3 bt b--white">
-				    	{sumNutrient("carbs")}
-				    </td>
-				    <td className="pv3 b tc pr3 bt b--white">
-				    	{sumNutrient("protein")}
-				    </td>
-		        </tr>
-		      </tbody>
-		    </table>
-		    <p className="w4 b ph3 pv2 center tc light-blue ba br2 b--light-blue bg-transparent grow pointer f6"
-		       onClick={addAnotherFood}>
-		    	Add Food
-		    </p>
-		  </div>
+		<div className="table-container">
+			<div className="overflow-auto">
+				<h1 className="tc white mb5">Your Daily Nutrition History</h1>
+				<table className="f6 w-100 mw8 center" cellSpacing="0">
+					<thead>
+						<tr>
+							<th className="fw6 tc white pb3 pr3 nutrition-table-header">
+								Food
+							</th>
+							<th className="fw6 tc white pb3 pr3 nutrition-table-header">
+								Calories
+							</th>
+							<th className="fw6 tc white pb3 pr3 nutrition-table-header">
+								Fat
+							</th>
+							<th className="fw6 tc white pb3 pr3 nutrition-table-header">
+								Carbs
+							</th>
+							<th className="fw6 tc white pb3 pr3 nutrition-table-header">
+								Protein
+							</th>
+						</tr>
+					</thead>
+					<tbody className="lh-copy">
+						{dailyFoods.map((food, i) => {
+							return (
+								<tr key={i}>
+									<td className="pv3 tc pr3 nutrition-row">
+										{food.food}
+									</td>
+									<td className="pv3 tc pr3 nutrition-row">
+										{food.calories}
+									</td>
+									<td className="pv3 tc pr3 nutrition-row">
+										{food.fat}
+									</td>
+									<td className="pv3 tc pr3 nutrition-row">
+										{food.carbs}
+									</td>
+									<td className="pv3 tc pr3 nutrition-row">
+										{food.protein}
+									</td>
+									<td className="pv3 tc pr3">
+										<button
+											className="br-100 bw1 b pointer ba b--washed-red bg-transparent washed-red"
+											value={i}
+											data-id={food.food_id}
+											onClick={deleteFood}
+										>
+											x
+										</button>
+									</td>
+								</tr>
+							);
+						})}
+						<tr>
+							<td className="pv3 b tc pr3 bt total-row">
+								Daily Total
+							</td>
+							<td className="pv3 b tc pr3 bt total-row">
+								{sumNutrient("calories")}
+							</td>
+							<td className="pv3 b tc pr3 bt total-row">
+								{sumNutrient("fat")}
+							</td>
+							<td className="pv3 b tc pr3 bt total-row">
+								{sumNutrient("carbs")}
+							</td>
+							<td className="pv3 b tc pr3 bt total-row">
+								{sumNutrient("protein")}
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				<p
+					className="w4 b ph3 pv2 center tc light-blue ba br2 b--light-blue bg-transparent grow pointer f6"
+					onClick={addAnotherFood}
+				>
+					Add Food
+				</p>
+			</div>
 		</div>
-		);
-}
+	);
+};
 
 export default NutritionTable;
